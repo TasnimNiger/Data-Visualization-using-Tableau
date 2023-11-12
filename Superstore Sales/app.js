68f8a58fbd7ec33b4f37d3660d5c0cdb9b9837a7
Tasnim Niger <85071596+TasnimNiger@users.noreply.github.com>
2023-11-12 00:18:16 +0000

    Add files via upload

---
+++ Superstore Sales/app.js
@@ -0,0 +1,49 @@
+console.log('Is this working?');
+
+let viz;
+
+//Add Share Link to Tableau Public in here
+const url = "https://public.tableau.com/shared/TB2HKX3GB?:display_count=n&:origin=viz_share_link";
+
+const vizContainer = document.getElementById('vizContainer');
+const options = {
+    hideTabs: true,
+    height: 1000,
+    width: 1200,
+    onFirstInteraction: function() {
+        workbook = viz.getWorkbook();
+        activeSheet = workbook.getActiveSheet();
+        console.log("My dashboard is interactive");
+    }
+};
+
+//create a function to generate the viz element
+function initViz() {
+    console.log('Executing the initViz function!');
+    viz = new tableau.Viz(vizContainer, url, options);
+}
+
+// run the initViz function when the page loads
+document.addEventListener("DOMContentLoaded", initViz);
+
+const exportPDF = document.getElementById('exportPDF');
+const exportImage = document.getElementById('exportImage');
+
+
+//click on the pdf button to generate pdf of dashboard
+function generatePDF() {
+    viz.showExportPDFDialog()
+}
+
+exportPDF.addEventListener("click", function () {
+    generatePDF();
+  });
+
+//click on image to generate image of dashboard
+function generateImage() {
+    viz.showExportImageDialog()
+}
+
+exportImage.addEventListener("click", function () {
+    generateImage();
+  });
